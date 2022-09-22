6b8045e6956e4fea405542b2398ed4f6afc88c31
CentPeter <solomonpeter1905@gmail.com>
2022-09-22 03:53:11 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,16 @@ void print_number(int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
 	else if (n > 0)
 		n *= -1;
 	else
-		_putchar('-');
+		putchar('-');
 	if ((n / 10) != 0)
 	{
 		print_number((n / 10) * -1);
 	}
-	_putchar((n % 10) * -1 + '0');
+	putchar((n % 10) * -1 + '0');
 }
