94cbb256281c0e2c58a3cb04367568247340cc42
CentPeter <solomonpeter1905@gmail.com>
2022-09-26 12:12:03 -0700

    Task 5  strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,31 @@
 #include "main.h"
-
 /**
  * _strstr - first occurence of the substring needle in string haystack
  * @haystack: main string to be examined
  * @needle: searched in haystack
+ *
  * Return: always 0
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	char *str1, *str2; /*Declaring variables*/
+	unsigned int i = 0, j = 0;
 
-	while (*haystack != '\0')
+	while (*haystack[i])
 	{
-		str1 = haystack; /*values*/
-		str2 = needle;
-
-		/*Star WHILE*/
-		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			haystack++;
-			str2++;
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
-		if (*str2 == '\0')
-			return (str2);
-		haystack = str1 + 1;
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
 	return (0);
 }
