262c0f92b99b642f4547c0444160764ae7265d23
CentPeter <solomonpeter1905@gmail.com>
2022-10-03 02:13:01 -0700

    Task 1  imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - pointer to a newly alloted
+ *  space in memory that contains copy of string
+ *  passed.
+ * @str: pointer to string being duplicated
+ *
+ * Return: NULL if the str is NULL.
+ * pointerto duplicated string on success
+ * NULL if memory was insufficient
+ */
+char *_strdup(char *str)
+{
+	char *nstr;
+	unsigned int len, i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
+
+}
