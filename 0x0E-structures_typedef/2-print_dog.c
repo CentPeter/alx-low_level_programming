ded73bae161ba72dd799882ce664f322f5373959
CentPeter <solomonpeter1905@gmail.com>
2022-10-10 05:55:57 -0700

    Task 2 unconditional love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,13 +19,13 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
-			print ("Name: %s\n", d->name);
+			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
-			print("Owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
