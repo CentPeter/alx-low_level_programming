c551e10f0dbaf43a72207db67aa9c89518a96a3f
CentPeter <solomonpeter1905@gmail.com>
2022-10-12 10:25:34 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	/* check if theres only one operator*/
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,4 @@
-include "3-calc.h"
+#include "3-calc.h"
 /**
  * op_add return the sum of 2 numbers
  * @a: integer
