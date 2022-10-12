12fa9f548d45f8b916a892d123d8f170c111175b
CentPeter <solomonpeter1905@gmail.com>
2022-10-12 04:08:31 -0700

    Task 0  What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_ name - to print a name
+ * @name: points to name
+ * @f: function of pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name;
+}
