efb8833342e302e0f7956e4812f8cfabcd866a55
CentPeter <solomonpeter1905@gmail.com>
2022-10-12 15:24:17 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,33 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * main - Run the program
- * @argc: Count of args given
- * @argv: Array of arg strings
- *
- * Return: 0 on success, print Error and exit with 98 if incorrect
- * number of args, exit with 99 if operator given is incorrect
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int);
-	int num1, num2, res;
+	int a, b, c;
+	int (*f)(int, int);
 
-	if (argc == 4)
-	{
-		if (argv[2][1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		func = get_op_func(argv[2]);
-		if (func == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		res = func(num1, num2);
-		printf("%d\n", res);
-		return (0);
-	}
-	else
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
 }
